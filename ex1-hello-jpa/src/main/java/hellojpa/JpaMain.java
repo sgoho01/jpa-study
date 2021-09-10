8af9f0e6c05579d67b6d9e13571e8d52ef852e71
ghsong <ghsong@greenlamp.kr>
2021-09-10 12:09:35 +0900

    create jpa query(insert,select,delete,update)

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -2,7 +2,9 @@ package hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.Arrays;
 
 /**
  * @author : Gunho.Song
@@ -14,8 +16,37 @@ public class JpaMain {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
         EntityManager em = emf.createEntityManager();
-        // code
-        em.close();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            // insert code
+            /*Member member = new Member();
+            member.setId(2L);
+            member.setName("helloB");
+
+            em.persist(member);*/
+
+            // select code
+            /*Member findMember = em.find(Member.class, 1L);
+            System.out.println("findMember id : " + findMember.getId());
+            System.out.println("findMember name : " + findMember.getName());*/
+
+            // delete code
+            /*Member findMember = em.find(Member.class, 1L);
+            em.remove(findMember);*/
+
+            // update code
+            Member findMember = em.find(Member.class, 1L);
+            findMember.setName("helloJpa");
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
 
         emf.close();
     }
