f62167f0a6a7437db5207680a7cd1204f54dc6ff
ghsong <ghsong@greenlamp.kr>
2021-09-24 17:10:23 +0900

    add key mapping

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -23,11 +23,25 @@ public class JpaMain {
 
         try {
             // insert code
-            /*Member member = new Member();
-            member.setId(2L);
-            member.setName("helloB");
+            Member member1 = new Member();
+            member1.setUsername("helloAA");
 
-            em.persist(member);*/
+            Member member2 = new Member();
+            member2.setUsername("helloBB");
+
+            Member member3 = new Member();
+            member3.setUsername("helloCC");
+
+            System.out.println("1 =======================");
+            em.persist(member1);
+            em.persist(member2);
+            em.persist(member3);
+
+            System.out.println("member1 id : " + member1.getId());
+            System.out.println("member2 id : " + member2.getId());
+            System.out.println("member3 id : " + member3.getId());
+
+            System.out.println("2 =======================");
 
             // select code
             /*Member findMember = em.find(Member.class, 1L);
@@ -43,16 +57,17 @@ public class JpaMain {
             findMember.setName("helloJpa");*/
 
             // jpql
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+            /*List<Member> result = em.createQuery("select m from Member as m", Member.class)
                     .setFirstResult(5)
                     .setMaxResults(8)
                     .getResultList();
-            for (Member member : result) {
-                System.out.println("member = " + member.getName());
-            }
+            for (Member member1 : result) {
+                System.out.println("member1 = " + member1.getName());
+            }*/
 
             tx.commit();
         } catch (Exception e) {
+            e.printStackTrace();
             tx.rollback();
         } finally {
             em.close();
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,17 +1,31 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 /**
  * @author : Gunho.Song
  * @date : 2021-09-10
  */
 @Entity
+@SequenceGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        sequenceName = "MEMBER_SEQ",
+        initialValue = 1, allocationSize = 50
+)
 public class Member {
+
     @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "MEMBER_SEQ_GENERATOR"
+    )
     private Long id;
-    private String name;
+
+    @Column(name = "name", nullable = false)
+    private String username;
+
+    public Member() {
+    }
 
     public Long getId() {
         return id;
@@ -21,11 +35,11 @@ public class Member {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
     }
 }
