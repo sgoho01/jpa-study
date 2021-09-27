f9bd9657f155bce69b134af8fd1c1b1d5f6fd490
ghsong <ghsong@greenlamp.kr>
2021-09-27 17:40:23 +0900

    create jpashop project

---
+++ jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,46 @@
+package jpabook.jpashop;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+/**
+ * @author : Gunho.Song
+ * @date : 2021-09-27
+ */
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setName("member1");
+            em.persist(member);
+
+            Order order = new Order();
+            order.setMember(member);
+
+            em.find(Order.class, order)
+
+
+            tx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
---
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderStatus.java
@@ -0,0 +1,9 @@
+package jpabook.jpashop.domain;
+
+/**
+ * @author : Gunho.Song
+ * @date : 2021-09-27
+ */
+public enum OrderStatus {
+    ORDER, CANCEL
+}
